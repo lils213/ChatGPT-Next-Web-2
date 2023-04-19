b921c29fe5d8cd0b3983f475ec35ed73750b1616
issenn <28648060+issenn@users.noreply.github.com>
2023-04-19 17:11:47 +0800

    feat: init

---
+++ app/constant.ts
@@ -0,0 +1,8 @@
+export const OWNER = "issenn";
+export const REPO = "";
+export const REPO_URL = `https://github.com/${OWNER}`;
+export const ISSUE_URL = `https://github.com/${OWNER}`;
+export const UPDATE_URL = `https://github.com/${OWNER}`;
+export const FETCH_COMMIT_URL = ``;
+export const FETCH_TAG_URL = ``;
+export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
